db5f87d6b97fbdb1e5b76fabb077821be5da729d
hubcarl <hubcarl@126.com>
2017-09-18 15:16:45 +0800

    refactor:build script to webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+'use strict';
+
+exports.framework = 'vue';
+
+exports.entry = {
+  include: 'test/web/page',
+  exclude: ['test/web/page/test']
+};
+
+exports.alias = {
+  asset: 'test/web/asset',
+  app: 'test/web/framework/vue/app.js',
+  component: 'test/web/component',
+  framework: 'test/web/framework',
+  store: 'test/web/store'
+};
+
+exports.options = {};
+
+exports.loaders = {
+  eslint: {
+    options: {
+      fix: true
+    }
+  },
+  babel:{
+    options:{
+      presets: ['es2015', 'stage-2'],
+      plugins: [
+        'add-module-exports'
+      ],
+      comments: false
+    }
+  },
+  options:{
+
+  }
+};
+
+exports.plugins = {};
