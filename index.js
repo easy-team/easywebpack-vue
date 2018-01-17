3c5023b040878187ec63c477b9d0768bd18e1a70
sky <hubcarl@126.com>
2018-01-17 15:23:46 +0800

    feat: add getWebWebpackConfig and getNodeWebpackConfig alias method

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ exports.WebpackServerBuilder = require('./lib/server');
 exports.getWebpackConfig = (config, option) => {
   return EasyWebpack.WebpackBuilder.getWebpackConfig(config, [exports.WebpackClientBuilder, exports.WebpackServerBuilder], option);
 };
+
+exports.getWebWebpackConfig = (config = {}, option = {}) => {
+  return exports.WebpackBuilder.getWebpackConfig(config, [exports.WebpackClientBuilder], option);
+};
+
+exports.getNodeWebpackConfig = (config = {}, option = {}) => {
+  return exports.WebpackBuilder.getWebpackConfig(config, [exports.WebpackServerBuilder], option);
+};
