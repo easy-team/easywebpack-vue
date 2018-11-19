ab6f95503d039a4489f5d9133b4ace99a0a631b0
beliefgp <ggjqzjgp103@qq.com>
2018-11-19 15:32:37 +0800

    fix: 解决 ts-loader 构建错误问题

--- config/loader.js
+++ config/loader.js
@@ -5,6 +5,17 @@ exports.vue = {
   use() {
     const options = this.createFrameworkLoader('vue-style-loader');
     options.transformToRequire = { img: ['url', 'src'] };
+
+    // https://github.com/TypeStrong/ts-loader/pull/782
+    // vue-loader 14 版本中，需在 options 重复配置 ts-loader，升级 15 以后可移除
+    if (this.loaders.typescript.enable) {
+      options.loaders = options.loaders || {};
+      this.loaders.typescript.use.forEach(loader => {
+        const label = this.utils.getLoaderLabel(loader);
+        options.loaders[label] = loader;
+      });
+    }
+
     return [
       {
         loader: 'vue-loader',
